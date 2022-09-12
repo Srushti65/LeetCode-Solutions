9fc64fe9187d3726f232b02352582bcfcd80c7a1
Srushti Menpara <67777427+Srushti65@users.noreply.github.com>
2022-09-12 21:45:12 +0530

    Time: 63 ms (12.42%), Space: 31.1 MB (52.17%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) 
     {
-        int ans = -1;
+        int rsum = 0;
+        int lsum = 0;
+        int index=-1;
         int n = nums.size();
-        
-        vector<int> prefix(n);
-        vector<int> suffix(n);
-        
-        prefix[0] = nums[0];
-        for(int i=1;i<n;i++)
-        {
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-        
-        suffix[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            suffix[i] = suffix[i+1]+nums[i];
-        }
         
         for(int i=0;i<n;i++)
         {
-            if(prefix[i]==suffix[i])
-                return i;
+            rsum += nums[i];
         }
-        return ans;
+        cout<<rsum<<endl;
+        
+        for(int i=0;i<n;i++)
+        {
+            rsum -= nums[i];
+            
+           // cout<<"rsum = "<<rsum<<endl;
+            
+            if(rsum==lsum) return i;
+            
+            lsum+= nums[i];
+            //cout<<"lsum = "<<lsum<<endl;
+        }
+        return index;
     }
 };
